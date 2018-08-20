957de39aaf050db4a8e9acf8d92834512ec6e7dc
Mobarak Ali <mobarak.ali@gmail.com>
2018-08-20 23:56:37 +0600

    Undefined variable makes the logic true!

--- palyground/mdn/guide/02.js
+++ palyground/mdn/guide/02.js
@@ -1,6 +1,13 @@
-var input;
-if(input === undefined){
-  console.log('TRUE');
-} else {
-  console.log('FALSE')
+"use strict";
+
+// var input;
+// if(input === undefined){
+//   console.log('TRUE');
+// } else {
+//   console.log('FALSE')
+// }
+
+var myArray = []
+if(!myArray[0]){
+  console.log("True!")
 }
