da4f09a6bf1a1da60414e9a91256b73917fb4456
Mobarak Ali <mobarak.ali@gmail.com>
2018-08-20 23:59:53 +0600

    Undefined can not add to a number! while text can!

--- palyground/mdn/guide/02.js
+++ palyground/mdn/guide/02.js
@@ -7,7 +7,14 @@
 //   console.log('FALSE')
 // }
 
-var myArray = []
-if(!myArray[0]){
-  console.log("True!")
-}
+// var myArray = []
+// if(!myArray[0]){
+//   console.log("True!")
+// }
+
+
+var a;
+console.log(a+2);  // Return NAN
+
+var b;
+console.log(b + " Ball");
