8158a02ee1c2b950dc232bf8829572d75f0b9650
Mobarak Ali <mobarak.ali@gmail.com>
2018-08-20 23:51:38 +0600

    use undefined to check wheather the variable has a value

--- palyground/mdn/guide/02.js
+++ palyground/mdn/guide/02.js
@@ -1,23 +1,6 @@
-/*
-// Grammar and types
-*/
-// JavaScript support unicode variable name
-var ভেরিয়েবল = 'জাভাস্ক্রিপ্টে ভেরিয়েবল ইউনিকোড ব্যবহার করে লেখা যায়!';
-// alert(ভেরিয়েবল);
-// ভেরিয়েবল ডিক্লিয়ারেশন
-var a = 1;
-let b = 2; // works inside a block scope
-const c = 3; // value never changes;
-
-// If variables are used before it decleared
-var d = 11;
-console.log(d); // d decleared early
-
-// console.log(e); // e not decleared
- e = 22; // a referenceError
-// may be halting the program
-
-
- // f not deleared
-console.log(f); // undefined
-var f = 5; //
+var input;
+if(input === undefined){
+  console.log('TRUE');
+} else {
+  console.log('FALSE')
+}
