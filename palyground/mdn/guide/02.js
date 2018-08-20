c300aab891e9fcddd98fbe87c9a1d6841551bb4f
Mobarak Ali <mobarak.ali@gmail.com>
2018-08-20 23:32:37 +0600

    working with step by step on javascript guide on MDN

---
+++ palyground/mdn/guide/02.js
@@ -0,0 +1,20 @@
+/*
+// Grammar and types
+*/
+var ভেরিয়েবল = 'জাভাস্ক্রিপ্টে ভেরিয়েবল ইউনিকোড ব্যবহার করে লেখা যায়!';
+alert(ভেরিয়েবল);
+// ভেরিয়েবল ডিক্লিয়ারেশন
+var a = 1;
+// let b = 2; // works inside a block scope
+// const c = 3; // value never changes;
+a = 11;
+console.log(a);
+//b = 22;
+console.log(b);
+
+/*
+undefined
+*/
+/*
+undefined
+*/
---
+++ palyground/mdn/scratchpad.js
@@ -0,0 +1,20 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+(function(){
+  "use strict";
+  /* Start your code here */
+  function greetMe(yourName){
+  alert("Hello " + yourName);
+  }
+
+  greetMe("World!");
+  /* End of your code */
+  
+})()
